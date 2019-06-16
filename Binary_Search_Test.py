d92c51d86fb957ed65ac389798d9e900880bf81c
hungry5656 <2863880262@qq.com>
2019-06-16 18:14:22 +0800

    Add a test python file for Binary_Search.py

---
+++ Binary_Search_Test.py
@@ -0,0 +1,8 @@
+# ! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from Binary_Search import Binary_Search
+My_List = [1, 3, 5, 7, 9]
+if __name__ == "__main__":
+    print(Binary_Search(My_List, 3))
+    print(Binary_Search(My_List, -1))
